9b0b7445516c3d808d9390b799cbe97d3da2afab
jay_g <jay_g@kr.accommate.com>
2020-06-18 13:50:16 +0900

    2020 06 18

--- apps/in_queue/views.py
+++ apps/in_queue/views.py
@@ -1,15 +1,10 @@
-import json
-from datetime import datetime
-
 from django.http import HttpResponseNotAllowed
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import View
 
 from apps.in_queue.http_response import HttpJsonResponse
-from apps.in_queue.vos import InQueue
-from apps.third_party.database.mongo_dao import MongoDAO
-from apps.third_party.database.mongo_db import MongoDB
+from apps.third_party.database.collections.in_queue import Mongo_InQueue
 from apps.third_party.util.colorful import print_red
 from apps.third_party.util.exception import print_exception
 
@@ -20,66 +15,30 @@ class InQueue(View):
     permitted_methods = ['get', 'post']
 
     ###############################################################################
-    # GET
-    ###############################################################################
     def get(self, request, *args, **kwargs):
-        mongo = MongoDB()
         try:
-            query_result = mongo.find_list('in_queue', query = { })
+            result = Mongo_InQueue().query('get_list')
 
         except Exception as e:
             print_red(e.__class__, e.__str__())
             return HttpJsonResponse.fail(e, e.__str__())
 
-        finally:
-            mongo.close()
-
-        return HttpJsonResponse.success_mongo({ 'code': '0000', 'data': query_result })
+        return HttpJsonResponse.success_mongo({ 'code': '0000', 'result': result })
 
     ###############################################################################
 
     ###############################################################################
-    # POST
-    ###############################################################################
-    def _get_datalist(self, data):
-        if not data: raise ValueError('Empty data')
-        result = json.loads(data)
-        return result.get('datalist')
-
-    def _setup_in_queue(self, datalist):
-        queues = []
-
-        for data in datalist:
-            queue = InQueue()
-            queue.packageCd = data.get('packageCd')
-            queue.productCd = data.get('productCd')
-            queue.productItemCd = data.get('productItemCd')
-            queue.productName = data.get('productName')
-            queue.productUnitPrice = float(data.get('productUnitPrice'))
-            queue.inOrderCd = data.get('inOrderCd')
-            queue.inOrderDate = datetime.strptime(data.get('inOrderDate'), '%Y-%m-%d %H:%M:%S')
-
-            queues.append(queue)
-
-        return queues
-
     def post(self, request, *args, **kwargs):
-        mongo = MongoDB()
         try:
-            datalist = self._get_datalist(request.POST.get('data'))
-            queues = self._setup_in_queue(datalist)
-            mongo.create_bulk('in_queue', [q.__dict__ for q in queues])
+            result = Mongo_InQueue().query('insert_list', data = request.POST.get('data'))
 
         except Exception as e:
             print_red(e.__class__, e.__str__())
             return HttpJsonResponse.fail(e, e.__str__())
 
-        finally:
-            mongo.close()
+        return HttpJsonResponse.success_mongo({ 'code': '0000', 'result': result })
 
-        return HttpJsonResponse.success({ 'code': '0000' })
-
-        ###############################################################################
+    ###############################################################################
 
 
 @method_decorator(csrf_exempt, name = 'dispatch')
@@ -89,13 +48,13 @@ class InQueueOne(View):
 
     def get(self, request, *args, **kwargs):
         try:
-            product = MongoDAO().query('get_productCd_one', productCd = self.kwargs.get('productCd'))
+            product = Mongo_InQueue().query('get_productCd_one', productCd = self.kwargs.get('productCd'))
 
         except Exception as e:
             print_exception()
             return HttpJsonResponse.fail(e, e.__str__())
 
-        return HttpJsonResponse.success_mongo({ 'code': '0000', 'data': product })
+        return HttpJsonResponse.success_mongo({ 'code': '0000', 'result': product })
 
     ###############################################################################
 
@@ -111,10 +70,12 @@ class InQueueOne(View):
 
     def delete(self, request, *args, **kwargs):
         try:
-            delete_count = MongoDAO().query('delete_productCd', productCd = self.kwargs.get('productCd'))
+            delete_count = Mongo_InQueue().query('delete_productCd', productCd = self.kwargs.get('productCd'))
 
         except Exception as e:
             print_exception()
             return HttpJsonResponse.fail(e, e.__str__())
 
-        return HttpJsonResponse.success({ 'code': '0000', 'delete_count': delete_count })
+        return HttpJsonResponse.success({ 'code': '0000', 'result': delete_count })
+
+    ###############################################################################
--- apps/third_party/database/mongo_dao.py
+++ apps/third_party/database/mongo_dao.py
@@ -1,5 +1,4 @@
 from apps.third_party.database.mongo_db import MongoDB
-from apps.third_party.util.exception import DBSelectNone
 
 
 class MongoDAO:
@@ -7,7 +6,7 @@ class MongoDAO:
     def query(self, query_type = None, **kwargs):
         try:
             if not query_type: raise Exception('query_type is empty')
-            if not hasattr(self, query_type): raise Exception('query_type is not proper')
+            if not hasattr(self, query_type): raise Exception('query_type is not declared')
 
         except Exception as e:
             raise e
@@ -24,28 +23,3 @@ class MongoDAO:
             mongo.close()
 
         return result
-
-    @staticmethod
-    def get_productCd_one(mongo, productCd = None):
-        try:
-            if not productCd: raise ValueError('Empty productCd')
-
-            query_result = mongo.find_one('in_queue', { 'productCd': productCd })
-            if not query_result: raise DBSelectNone('Non exist product')
-
-        except Exception as e:
-            raise e
-
-        return query_result
-
-    @staticmethod
-    def delete_productCd(mongo, productCd = None):
-        try:
-            if not productCd: raise ValueError('Empty productCd')
-
-            document = mongo.remove('in_queue', { 'productCd': productCd })
-
-        except Exception as e:
-            raise e
-
-        return document.deleted_count
