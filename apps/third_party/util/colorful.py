31597d22f344042bf56764ac72e61327f11bc283
jay_g <jay_g@kr.accommate.com>
2020-08-26 14:43:10 +0900

    소스 정리

--- apps/third_party/util/colorful.py
+++ apps/third_party/util/colorful.py
@@ -1,17 +1,6 @@
-class Colors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-    BACK_RED = '\033[41m'
-
-
 # print_to_server = lambda color, msg, *args: print(color, msg, *args, Colors.ENDC)
+from apps.third_party.util.constants import Colors
+
 
 def print_green(msg, *args):
     print(Colors.OKGREEN, msg, *args, Colors.ENDC)
