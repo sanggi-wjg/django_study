3433c67bdbe43613e29c4862e537c6de99c570f0
jay_g <jay_g@kr.accommate.com>
2020-11-23 15:47:06 +0900

    index 주가 지수 저장

---
+++ apps/model/index.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Index(models.Model):
+    id = models.AutoField(primary_key = True, db_column = 'id')
+
+    index_name = models.CharField(max_length = 45, null = False, db_column = 'index_name')
+    number = models.IntegerField(null = False, db_column = 'number')
+    date = models.DateField(null = False, db_column = 'date')
+
+    class Meta:
+        managed = False
+        db_table = 'index'
---
+++ apps/third_party/management/commands/register_index.py
@@ -0,0 +1,57 @@
+import FinanceDataReader as fdr
+
+from django.core.management import BaseCommand
+
+from apps.model.index import Index
+from apps.third_party.util.colorful import print_green
+from apps.third_party.util.utils import today_dateformat
+
+
+class Command(BaseCommand):
+    help = 'Register Indexes'
+
+    def add_arguments(self, parser):
+        parser.add_argument('index', type = str, help = 'Index Name... [KOSPI(KS11), KOSDAQ, ...]')
+        parser.add_argument('date', type = str, nargs = '?', default = '',
+                            help = 'Date to request (optional : If None, value will be today date format)')
+
+    def handle(self, *args, **options):
+        """
+        python manage.py register_index KS11 all
+        python manage.py register_index IXIC all
+        python manage.py register_index DJI all
+        python manage.py register_index US500 all
+        """
+        index_name = options['index'].upper()
+        if options['date'] == 'all':
+            start_date = '1970-01-01'
+        else:
+            start_date = today_dateformat(time_format = '%Y-%m-%d')
+
+        register(index_name, start_date)
+
+
+def convert_index_name(index_name):
+    return {
+        'KS11' : 'KOSPI',
+        'IXIC' : 'NASDAQ',
+        'DJI'  : 'DowJones',
+        'US500': 'S&P500',
+    }[index_name]
+
+
+def register(index_name: str, start_date: str):
+    index_df = fdr.DataReader(index_name, start_date)
+    index_name = convert_index_name(index_name)
+
+    for date, row in index_df.iterrows():
+        try:
+            Index.objects.get(index_name = index_name, date = date)
+
+        except Index.DoesNotExist:
+            print_green('[{} - {}] {}'.format(index_name, date, row['Close']))
+            Index.objects.create(
+                index_name = index_name,
+                number = row['Close'],
+                date = date
+            )
