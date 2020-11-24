3a3abf01fd622900d7b132a505c9057f3e6f7b13
jay_g <jay_g@kr.accommate.com>
2020-11-24 10:40:53 +0900

    plot 리팩토링

--- apps/third_party/plot/plt_utils.py
+++ apps/third_party/plot/plt_utils.py
@@ -1,28 +1,56 @@
+import os
+
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from pandas import DataFrame
+
+from apps.third_party.util.utils import validate_path
+from sample.settings import MEDIA_ROOT
 
 
 def plt_year_format():
     return mdates.YearLocator(), mdates.DateFormatter('%Y')
 
 
-def show_twinx_plot(df_1: DataFrame, df_1_label: str, df_2: DataFrame, df_2_label: str, show_year_format = False):
+def plt_year_month_format():
+    return mdates.MonthLocator(), mdates.DateFormatter('%Y-%M')
+
+
+def plt_path(filename: str):
+    path = os.path.join(MEDIA_ROOT, 'plt')
+    validate_path(path)
+
+    return os.path.join(path, filename)
+
+
+def show_twinx_plot(df_1: DataFrame, df_1_label: str, df_2: DataFrame, df_2_label: str, plot_format = None, if_filename: str = None):
     plt.rcParams["figure.figsize"] = (60, 20)
     plt.rcParams['font.size'] = 20
 
-    fig, ax1 = plt.subplots()
-    ax1.xaxis.set_major_locator(mdates.YearLocator())
-    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
+    # fig, ax1 = plt.subplots()
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+
+    if plot_format:
+        locator, formatter = plot_format()
+        ax1.xaxis.set_major_locator(locator)
+        ax1.xaxis.set_major_formatter(formatter)
 
     ax1.set_ylabel(df_1_label)
-    ax1.plot(df_1, color = 'blue', label = df_1_label)
-    ax1.legend()
+    line1 = ax1.plot(df_1, color = 'blue', label = df_1_label)
 
     ax2 = ax1.twinx()
     ax2.set_ylabel(df_2_label)
-    ax2.plot(df_2, color = 'red', label = df_2_label)
-    ax2.legend()
+    line2 = ax2.plot(df_2, color = 'red', label = df_2_label)
 
-    plt.show()
+    # ax1.legend(handles = (line1, line2), labels = (df_1_label, df_2_label), loc = 'upper right')
+    lns = line1 + line2
+    labs = [x.get_label() for x in lns]
+    ax1.legend(lns, labs, loc = 'upper left')
+
+    if if_filename is None:
+        plt.show()
+    else:
+        plt.savefig(plt_path(if_filename), bbox_inches = 'tight', pad_inches = 0.5)
+
     plt.close(fig)
--- apps/third_party/reports/test/test_report.py
+++ apps/third_party/reports/test/test_report.py
@@ -4,29 +4,28 @@ import pytest
 from apps.model.index import Index
 from apps.model.reports import Reports
 from apps.model.reports_name import ReportsName
-from apps.third_party.plot.plt_utils import show_plot
+from apps.third_party.plot.plt_utils import show_twinx_plot, plt_year_format
 
 
 @pytest.mark.django_db
 def test_report():
-    START_DATE = '2000-01-01'
-    FINISH_DATE = '2019-12-31'
+    for year in range(1980, 2021):
+        START_DATE, FINISH_DATE = '{}-01-01'.format(year), '{}-12-31'.format(year)
 
-    unempl_request = Reports.objects.values('number', 'date').filter(
-        report_id = ReportsName.objects.get(reports_name = '미국 주간 실업수당 청구건수'),
-        date__gte = START_DATE, date__lte = FINISH_DATE
-    ).order_by('date')
-    index = Index.objects.all()
-    kopsi = index.values('number', 'date').filter(index_name = 'KOSPI', date__gte = START_DATE, date__lte = FINISH_DATE).order_by('date')
-    nasdaq = index.values('number', 'date').filter(index_name = 'NASDAQ', date__gte = START_DATE, date__lte = FINISH_DATE).order_by('date')
+        unempl_request = Reports.objects.values('number', 'date').filter(
+            report_id = ReportsName.objects.get(reports_name = '미국 주간 실업수당 청구건수'),
+            date__gte = START_DATE, date__lte = FINISH_DATE
+        ).order_by('date')
 
-    print()
-    print(len(unempl_request), len(kopsi))
-    print(unempl_request)
-    print(kopsi)
+        index = Index.objects.all()
+        kopsi = index.values('number', 'date').filter(index_name = 'KOSPI', date__gte = START_DATE, date__lte = FINISH_DATE).order_by('date')
+        nasdaq = index.values('number', 'date').filter(index_name = 'NASDAQ', date__gte = START_DATE, date__lte = FINISH_DATE).order_by('date')
 
-    unempl_df = pd.DataFrame([x['number'] for x in unempl_request], index = [x['date'] for x in unempl_request], columns = ['UnEmployee_Request'])
-    kospi_df = pd.DataFrame([x['number'] for x in kopsi], index = [x['date'] for x in kopsi], columns = ['KOSPI'])
-    nasdaq_df = pd.DataFrame([x['number'] for x in nasdaq], index = [x['date'] for x in nasdaq], columns = ['NASDAQ'])
+        unempl_df = pd.DataFrame([x['number'] for x in unempl_request], index = [x['date'] for x in unempl_request], columns = ['UnEmployee_Request'])
+        kospi_df = pd.DataFrame([x['number'] for x in kopsi], index = [x['date'] for x in kopsi], columns = ['KOSPI'])
+        nasdaq_df = pd.DataFrame([x['number'] for x in nasdaq], index = [x['date'] for x in nasdaq], columns = ['NASDAQ'])
 
-    show_plot(kospi_df, unempl_df, nasdaq_df)
+        show_twinx_plot(
+            unempl_df, 'UnEmployee_Request', nasdaq_df, 'NASDAQ', plot_format = plt_year_format,
+            if_filename = '[{}_{}]_UnEmployee_Request_NASDAQ.png'.format(START_DATE, FINISH_DATE)
+        )
